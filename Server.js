f84eb77c52efa52a3fc6be023f890a2b852f217b
Jaehyeok65 <62687875+Jaehyeok65@users.noreply.github.com>
2021-08-07 19:22:32 +0900

    서버 구현 (mysql과의 연동)

---
+++ Server.js
@@ -0,0 +1,279 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const multer = require('multer');
+
+var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : 'asfg1245!',
+    database : 'user',
+});
+
+ connection.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static("public"));
+ 
+// http://localhost:4000/ 으로 접속 시 응답메시지 출력
+app.get('/', (req,res) => {
+    res.send('Server Response Success');
+})
+
+app.post("/idplz", (req,res)=>{
+    const user_id = req.body.user_id;
+    const user_password = req.body.user_password;
+    const user_name = req.body.user_name;
+    //console.log(req.body);
+    connection.query("INSERT INTO userinformation (user_id,user_password,user_name) values (?,?,?)",[user_id,user_password,user_name],
+    function(err,rows,fields){
+        if(err){
+            console.log("실패");
+            // console.log(err);
+        }else{
+            console.log("성공");
+            res.send(rows);
+            //console.log(rows);
+        };
+    });
+
+    
+});
+
+app.post("/callbody", (req,res)=>{
+    connection.query("SELECT * FROM userinformation",
+    function(err,rows,fields){
+        if(err){
+            console.log("불러오기 실패");
+        }else{
+            console.log("불러오기 성공");
+            //console.log(rows);
+            res.send(rows);
+        }
+    })
+})
+
+app.post("/callnovel", (req,res)=>{
+    connection.query("SELECT * FROM novelinformation",
+    function(err,rows,fields){
+        if(err){
+            console.log("로드 실패");
+        }else{
+            console.log("로드 성공");
+            //console.log(rows);
+            res.send(rows);
+        }
+    })
+})
+
+const storage = multer.diskStorage({
+    destination: "C:/Users/wkkl2/reactproject/public/assets",
+    filename: function(req, file, cb) {
+      cb(null, file.originalname);
+    }
+  });
+
+  const upload = multer({
+    storage: storage,
+  });
+
+  
+  app.post("/uploaded", upload.single("file"), function(req, res, next) {
+      console.log(req.file.filename);
+       res.send(req.file.filename);
+  });
+
+    app.post("/callwebtoon", (req,res)=>{
+    connection.query("SELECT * FROM webtooninformation",
+    function(err,rows,fields){
+        if(err){
+            console.log("로드 실패");
+        }else{
+            console.log("로드 성공");
+            //console.log(rows);
+            res.send(rows);
+        }
+    })
+    })
+
+    app.post("/findid", (req,res)=>{
+        console.log(req.body);
+        connection.query("SELECT user_id,user_password FROM userinformation where user_name = ?",[req.body.user_name],
+        function(err,rows,fields){
+            if(err){
+                console.log("로드 실패");
+            }else{
+                console.log("로드 성공");
+                console.log(rows);
+                res.send(rows);
+            }
+        })
+    })
+
+    app.post("/novelrate", (req,res)=>{
+        //console.log(req.body);
+        connection.query("UPDATE novelinformation SET novel_rate = (novel_rate + ?)/2 WHERE novel_name = ?",[req.body.novel_rate,req.body.novel_name],
+        function(err,rows,fields){
+            if(err){
+                console.log("로드 실패");
+            }else{
+                console.log("로드 성공");
+                console.log(rows);
+                res.send(rows);
+            }
+        })
+        })
+
+        app.post("/webtoonrate", (req,res)=>{
+            //console.log(req.body);
+            connection.query("UPDATE webtooninformation SET webtoon_rate = (webtoon_rate + ?)/2 WHERE webtoon_name = ?",[req.body.webtoon_rate,req.body.webtoon_name],
+            function(err,rows,fields){
+                if(err){
+                    console.log("로드 실패");
+                }else{
+                    console.log("로드 성공했다.");
+                    //console.log(rows);
+                    res.send(rows);
+                }
+            })
+            })
+
+app.post("/ranknovel", (req,res)=>{
+    connection.query("SELECT * FROM novelinformation ORDER BY novel_view DESC limit 3;",
+    function(err,rows,fields){
+        if(err){
+            console.log("로드 실패");
+        }else{
+            console.log("로드 성공");
+            //console.log(rows);
+            res.send(rows);
+        }
+    })
+})
+
+app.post("/rankwebtoon", (req,res)=>{
+    connection.query("SELECT * FROM webtooninformation ORDER BY webtoon_view DESC limit 3;",
+    function(err,rows,fields){
+        if(err){
+            console.log("로드 실패");
+        }else{
+            console.log("로드 성공");
+            //console.log(rows);
+            res.send(rows);
+        }
+    })
+})
+
+app.post("/webtoonview", (req,res)=>{
+    connection.query("UPDATE webtooninformation SET webtoon_view = webtoon_view + ? WHERE webtoon_name = ?",[req.body.webtoon_view,req.body.webtoon_name],
+    function(err,rows,fields){
+        if(err){
+            console.log("로드 실패");
+        }else{
+            console.log("로드 성공");
+            //console.log(rows);
+            res.send(rows);
+        }
+    })
+})
+
+app.post("/novelview", (req,res)=>{
+    connection.query("UPDATE novelinformation SET novel_view = novel_view + ? WHERE novel_name = ?",[req.body.novel_view,req.body.novel_name],
+    function(err,rows,fields){
+        if(err){
+            console.log("로드 실패");
+        }else{
+            console.log("로드 성공");
+            //console.log(rows);
+            res.send(rows);
+        }
+    })
+})
+
+app.post("/noveldelete", (req,res)=>{
+    connection.query("DELETE FROM novelinformation WHERE novel_name = ?",[req.body.novel_name],
+    function(err,rows,fields){
+        if(err){
+            console.log("로드 실패");
+        }else{
+            console.log("로드 성공");
+            //console.log(rows);
+        }
+    })
+})
+
+app.post("/webtoondelete", (req,res)=>{
+    connection.query("DELETE FROM webtooninformation WHERE webtoon_name = ?",[req.body.webtoon_name],
+    function(err,rows,fields){
+        if(err){
+            console.log("로드 실패");
+        }else{
+            console.log("로드 성공");
+            //console.log(rows);
+        }
+    })
+})
+
+app.post("/noveladd", (req,res)=>{
+    connection.query("INSERT INTO novelinformation (novel_name, novel_view, novel_rate, novel_link, novel_img) VALUES (?,?,?,?,?)",[req.body.novel_name,req.body.novel_view,req.body.novel_rate,req.body.novel_link,req.body.novel_img],
+    function(err,rows,fields){
+        if(err){
+            console.log("로드 실패");
+        }else{
+            console.log("로드 성공");
+            //console.log(rows);
+        }
+    })
+})
+
+app.post("/webtoonadd", (req,res)=>{
+    connection.query("INSERT INTO webtooninformation (webtoon_name, webtoon_view, webtoon_rate, webtoon_link, webtoon_img) VALUES (?,?,?,?,?)",[req.body.webtoon_name,req.body.webtoon_view,req.body.webtoon_rate,req.body.webtoon_link,req.body.webtoon_img],
+    function(err,rows,fields){
+        if(err){
+            console.log("로드 실패");
+        }else{
+            console.log("로드 성공");
+            //console.log(rows);
+        }
+    })
+})
+
+
+
+
+app.post("/islogin", (req,res)=>{
+    connection.query("SELECT user_id, user_password FROM userinformation where user_id = ?",[req.body.user_id],
+    function(err,rows,fields) {
+        if(err) {
+            console.log("오류가 발생했습니다.");
+        }
+        else {
+            res.send(rows);
+        }
+    }
+    );
+})
+
+app.post("/checklogin", (req,res)=>{
+    connection.query("SELECT user_id FROM userinformation where user_id = ?",[req.body.user_id],
+    function(err,rows,fields) {
+        if(err) {
+            console.log("오류가 발생했습니다.");
+        }
+        else {
+            res.send(rows);
+        }
+    }
+    );
+})
+
+ 
+app.listen(PORT, () => {
+    console.log(`Server run : http://localhost:${PORT}/`)
+})
+
